720fc60e445ddac4d9fce529ffbfac6357fc791d
Soham Salkar <salkarsoham2@gmail.com>
2021-03-22 17:35:58 +0530

    Create NosSteps.java

---
+++ NosSteps.java
@@ -0,0 +1,48 @@
+
+//https://www.hackerrank.com/challenges/ctci-recursive-staircase/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=recursion-backtracking
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the stepPerms function below.
+    static int stepPerms(int n) {
+        if (n == 1)
+            return 1;
+        else if (n == 2)
+            return 2;
+        else if (n == 3)
+            return 4;
+        else 
+            return (stepPerms(n-1)+stepPerms(n-2)+stepPerms(n-3));       
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int s = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int sItr = 0; sItr < s; sItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int res = stepPerms(n);
+
+            bufferedWriter.write(String.valueOf(res));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
