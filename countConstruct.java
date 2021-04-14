a80ebd03cc0fbe303dcf9f9493d3d7825154f9a4
Soham Salkar <salkarsoham2@gmail.com>
2021-04-14 17:22:13 +0530

    Create countConstruct.java

---
+++ countConstruct.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+
+public class countConstruct {
+
+    public static int howmany(String tar, String[] wor) {
+        if (memo.containsKey(tar))
+            return memo.get(tar);
+        if (tar == "")
+            return 1;
+
+        int tc = 0;
+
+        for (String w : wor) {
+            if (tar.indexOf(w) == 0) {
+                int baki = howmany(tar.substring(w.length()), wor);
+                tc += baki;
+            }
+        }
+
+        memo.put(tar, tc);
+        return tc;
+    }
+
+    static Map<String, Integer> memo = new HashMap<>();
+    // final static String[] wor = { "atebo", "ska", "boar", "rd", "sk", "a",
+    // "t","e", "board" };
+    final static String[] wor = { "a", "b", "c", "ab", "bc", "abc" };
+
+    public static void main(String[] args) {
+
+        System.out.println(howmany("abcd", wor));
+        System.out.println(memo);
+    }
+
+}
