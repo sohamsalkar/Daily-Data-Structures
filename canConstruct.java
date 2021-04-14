755c168cc4e4de435935a8c5e5d79e67a6ea3b3c
Soham Salkar <salkarsoham2@gmail.com>
2021-04-14 17:22:10 +0530

    Update canConstruct.java

--- canConstruct.java
+++ canConstruct.java
@@ -4,24 +4,32 @@ import java.lang.*;
 public class canConstruct {
 
     public static boolean canwe(String tar, String[] wor) {
+        if (memo.containsKey(tar))
+            return memo.get(tar);
         if (tar == "")
             return true;
 
         for (String e : wor) {
             if (tar.indexOf(e) == 0) {
                 String suf = tar.substring(e.length());
-                if (canwe(suf, wor) == true)
+                if (canwe(suf, wor) == true) {
+                    memo.put(tar, true);
                     return true;
+                }
+
             }
         }
+        memo.put(tar, false);
         return false;
     }
 
+    static Map<String, Boolean> memo = new HashMap<>();
     final static String[] wor = { "e", "ee", "eee", "eeee" };
 
     public static void main(String[] args) {
 
         System.out.println(canwe("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet", wor));
+        System.out.println(memo);
     }
 
 }
