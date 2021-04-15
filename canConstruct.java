b1f1afad3a95d25dedd6308772b94e6520d9000e
Soham Salkar <salkarsoham2@gmail.com>
2021-04-15 14:07:44 +0530

    Update canConstruct.java

--- canConstruct.java
+++ canConstruct.java
@@ -29,7 +29,7 @@ public class canConstruct {
     public static void main(String[] args) {
 
         System.out.println(canwe("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet", wor));
-        System.out.println(memo);
+        // System.out.println(memo);
     }
 
 }
