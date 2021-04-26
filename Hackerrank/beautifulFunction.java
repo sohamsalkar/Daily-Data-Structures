2f8a3f548a3e9ae221f0b61900f3d6a2889e2e59
Soham Salkar <salkarsoham2@gmail.com>
2021-04-26 23:43:54 +0530

    Create beautifulFunction.java

---
+++ Hackerrank/beautifulFunction.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.lang.Math;
+
+class Solution {
+    public static int beautifulFunction(int N) {
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N;
+        N = scan.nextInt();
+        int result;
+        result = beautifulFunction(N);
+        System.out.print(result);
+        return;
+    }
+}
