33b96fe2300ff0eb7d3a85fcc3c22701a972b5a0
Soham Salkar <salkarsoham2@gmail.com>
2021-05-31 19:53:12 +0530

    Update 220.contains-duplicate-iii.java

--- LeetCode/220.contains-duplicate-iii.java
+++ LeetCode/220.contains-duplicate-iii.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * @lc app=leetcode id=220 lang=java
  *
@@ -6,19 +9,31 @@
 
 // @lc code=start
 class Solution {
+    private long getID(long x, long w) {
+        return x < 0 ? (x + 1) / w - 1 : x / w;
+    }
+
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        boolean flag = false;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; Math.abs(i - j) <= k && j < nums.length; j++) {
-                long q1 = nums[i];
-                long q2 = nums[j];
-                if (Math.abs(q1 - q2) <= t) {
-                    flag = true;
-                    break;
-                }
-            }
+        if (t < 0)
+            return false;
+        Map<Long, Long> d = new HashMap<>();
+        long w = (long) t + 1;
+        for (int i = 0; i < nums.length; ++i) {
+            long m = getID(nums[i], w);
+
+            if (d.containsKey(m))
+                return true;
+            if (d.containsKey((m - 1)) && Math.abs(nums[i] - d.get(m - 1)) < w)
+                return true;
+            if (d.containsKey((m + 1)) && Math.abs(nums[i] - d.get(m + 1)) < w)
+                return true;
+            System.out.println(" m " + m + " nums " + nums[i]);
+            d.put(m, (long) nums[i]);
+            if (i >= k)
+                d.remove(getID(nums[i - k], w));
+
         }
-        return flag;
+        return false;
 
     }
 }
